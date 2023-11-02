ec482f3fad4c54d52288bcca20a7df2caa821b84
Dario Della Cerra <114399993+DarioDC95@users.noreply.github.com>
2023-11-02 15:34:13 +0100

    first commit

---
+++ src/main/java/hrport/project/main/controller/TestServlet.java
@@ -0,0 +1,32 @@
+package hrport.project.main.controller;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "FormServlet", urlPatterns = "/calculateServlet")
+public class TestServlet extends HttpServlet {
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+    protected void doGet(HttpServletRequest request, 
+      HttpServletResponse response)
+      throws ServletException, IOException {
+		
+        String ciao = "Hello from the server!";
+
+        // Set the JSON data as a request attribute
+        request.setAttribute("saluto", ciao);
+		
+		request.getRequestDispatcher("/WEB-INF/index.jsp").forward(request, response);
+    }
+}
+
---
+++ src/main/webapp/WEB-INF/index.jsp
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+ 
+<head>
+    <title>Person DB</title>
+</head>
+ 
+<body>
+    <table>
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+        </tr>
+        <tbody id="table-body">
+            <!-- Data will be inserted here -->
+        </tbody>
+    </table>
+ 
+<script type="text/javascript">
+	
+	let json = '<%=request.getAttribute("saluto")%>';
+
+</script>
+ 
+</body>
+</html>
