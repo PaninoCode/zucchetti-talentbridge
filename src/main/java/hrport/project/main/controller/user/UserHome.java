6cffeb210cae9c6ce4d3c5a363d4e19531a7b7f1
DarioDC95 <114399993+DarioDC95@users.noreply.github.com>
2023-11-07 12:32:41 +0100

    login progress

---
+++ src/main/java/hrport/project/main/controller/user/UserHome.java
@@ -0,0 +1,29 @@
+package hrport.project.main.controller.user;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+/**
+ * Servlet implementation class UserHome
+ */
+@WebServlet("/user/home")
+public class UserHome extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		HttpSession session = request.getSession();
+		request.setAttribute("data", session.getAttribute("user"));
+		request.getRequestDispatcher("/WEB-INF/test.jsp").forward(request, response);
+	}
+
+}
