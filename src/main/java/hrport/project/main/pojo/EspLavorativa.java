19747a9ada6d9c855c13f42a7abf7ccc5f9a5711
DarioDC95 <114399993+DarioDC95@users.noreply.github.com>
2023-11-13 17:47:24 +0100

    in progress

---
+++ src/main/java/hrport/project/main/pojo/EspLavorativa.java
@@ -0,0 +1,14 @@
+package hrport.project.main.pojo;
+
+import java.time.LocalDate;
+
+public class EspLavorativa {
+	
+	private Integer idCv;
+	private String azienda;
+	private LocalDate dInizio;
+	private LocalDate dFine;
+	private String posizione;
+	private String funzione;
+	private String indirizzo;
+}
