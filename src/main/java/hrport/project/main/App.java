daf7b5dd8a69e9d48c57bdc67dde601accc4ede5
DarioDC95 <114399993+DarioDC95@users.noreply.github.com>
2023-11-14 12:58:14 +0100

    changed numbers with columnNames in UtenteService

--- src/main/java/hrport/project/main/App.java
+++ src/main/java/hrport/project/main/App.java
@@ -6,14 +6,16 @@ import java.sql.ResultSetMetaData;
 import java.sql.Statement;
 import hrport.project.main.connectdb.ConnectDatabase;
 import hrport.project.main.pojo.*;
-import hrport.project.main.pojo.Utente;
 import hrport.project.main.service.UtenteService;
 
 public class App {
 
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("HelloWorld");
+		/*System.out.println("HelloWorld");
 		
 		try {
 			Quiz q= Quiz.initQuiz(1);
@@ -31,29 +33,14 @@ public class App {
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
-//        try {
-//        	Connection connection = ConnectDatabase.getConnection();
-//        	String query = "";
-//            Statement statement = connection.createStatement();
-//            query = "SELECT * FROM Utenti";
-//            ResultSet resultSet = statement.executeQuery(query);
-//            ResultSetMetaData rsmd = resultSet.getMetaData();
-//            int columnsNumber = rsmd.getColumnCount();
-//            while (resultSet.next()) {
-//                for (int i = 1; i <= columnsNumber; i++) {
-//                    if (i > 1) System.out.print(",  ");
-//                    String columnValue = resultSet.getString(i);
-//                    System.out.print(columnValue + " " + rsmd.getColumnName(i));
-//                }
-//                System.out.println("");
-//            }
-//            resultSet.close();
-//            statement.close();
-//            connection.close();
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
+		}*/
+        try {
+        	
+        	Utente utente = UtenteService.getUserByIdUtente(1);
+        	System.out.println(utente.getCognome());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 	}
 
 }
