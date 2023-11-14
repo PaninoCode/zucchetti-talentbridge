271f9a713a95878220a9cda1806b27bc5587a487
DarioDC95 <114399993+DarioDC95@users.noreply.github.com>
2023-11-14 17:36:46 +0100

    pojos done

--- src/main/java/hrport/project/main/pojo/EspLavorativa.java
+++ src/main/java/hrport/project/main/pojo/EspLavorativa.java
@@ -12,4 +12,66 @@ public class EspLavorativa {
 	private String posizione;
 	private String funzione;
 	private String indirizzo;
+	
+	public EspLavorativa(Integer idEl, Integer idCv, String azienda, LocalDate dInizio, LocalDate dFine,
+			String posizione, String funzione, String indirizzo) {
+		
+		setIdEl(idEl);
+		setIdCv(idCv);
+		setAzienda(azienda);
+		setdInizio(dInizio);
+		setdFine(dFine);
+		setPosizione(posizione);
+		setFunzione(funzione);
+		setIndirizzo(indirizzo);
+	}
+	
+	public Integer getIdEl() {
+		return idEl;
+	}
+	public void setIdEl(Integer idEl) {
+		this.idEl = idEl;
+	}
+	public Integer getIdCv() {
+		return idCv;
+	}
+	public void setIdCv(Integer idCv) {
+		this.idCv = idCv;
+	}
+	public String getAzienda() {
+		return azienda;
+	}
+	public void setAzienda(String azienda) {
+		this.azienda = azienda;
+	}
+	public LocalDate getdInizio() {
+		return dInizio;
+	}
+	public void setdInizio(LocalDate dInizio) {
+		this.dInizio = dInizio;
+	}
+	public LocalDate getdFine() {
+		return dFine;
+	}
+	public void setdFine(LocalDate dFine) {
+		this.dFine = dFine;
+	}
+	public String getPosizione() {
+		return posizione;
+	}
+	public void setPosizione(String posizione) {
+		this.posizione = posizione;
+	}
+	public String getFunzione() {
+		return funzione;
+	}
+	public void setFunzione(String funzione) {
+		this.funzione = funzione;
+	}
+	public String getIndirizzo() {
+		return indirizzo;
+	}
+	public void setIndirizzo(String indirizzo) {
+		this.indirizzo = indirizzo;
+	}
 }
--- src/main/java/hrport/project/main/pojo/Istruzione.java
+++ src/main/java/hrport/project/main/pojo/Istruzione.java
@@ -12,4 +12,66 @@ public class Istruzione {
 	private Integer voto;
 	private LocalDate dInizio;
 	private LocalDate dFine;
+	
+	public Istruzione(Integer idIst, Integer idCv, String titoloIstruzione, String istituto, String indirizzo,
+			Integer voto, LocalDate dInizio, LocalDate dFine) {
+		
+		setIdIst(idIst);
+		setIdCv(idCv);
+		setTitoloIstruzione(titoloIstruzione);
+		setIstituto(istituto);
+		setIndirizzo(indirizzo);
+		setVoto(voto);
+		setdInizio(dInizio);
+		setdFine(dFine);
+	}
+	
+	public Integer getIdIst() {
+		return idIst;
+	}
+	public void setIdIst(Integer idIst) {
+		this.idIst = idIst;
+	}
+	public Integer getIdCv() {
+		return idCv;
+	}
+	public void setIdCv(Integer idCv) {
+		this.idCv = idCv;
+	}
+	public String getTitoloIstruzione() {
+		return titoloIstruzione;
+	}
+	public void setTitoloIstruzione(String titoloIstruzione) {
+		this.titoloIstruzione = titoloIstruzione;
+	}
+	public String getIstituto() {
+		return istituto;
+	}
+	public void setIstituto(String istituto) {
+		this.istituto = istituto;
+	}
+	public String getIndirizzo() {
+		return indirizzo;
+	}
+	public void setIndirizzo(String indirizzo) {
+		this.indirizzo = indirizzo;
+	}
+	public Integer getVoto() {
+		return voto;
+	}
+	public void setVoto(Integer voto) {
+		this.voto = voto;
+	}
+	public LocalDate getdInizio() {
+		return dInizio;
+	}
+	public void setdInizio(LocalDate dInizio) {
+		this.dInizio = dInizio;
+	}
+	public LocalDate getdFine() {
+		return dFine;
+	}
+	public void setdFine(LocalDate dFine) {
+		this.dFine = dFine;
+	}
 }
