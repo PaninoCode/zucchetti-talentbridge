ce16be8069d3d4413beb552f2990fd0266ee6cc7
DarioDC95 <114399993+DarioDC95@users.noreply.github.com>
2023-11-15 17:58:13 +0100

    added method to get the profile in UserService

--- src/main/java/hrport/project/main/service/UtenteService.java
+++ src/main/java/hrport/project/main/service/UtenteService.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import hrport.project.main.connectdb.ConnectDatabase;
 import hrport.project.main.pojo.Posizione;
+import hrport.project.main.pojo.ProfiloUtente;
 import hrport.project.main.pojo.Utente;
 
 public class UtenteService {
@@ -98,6 +99,41 @@ public class UtenteService {
 			List<Posizione> positions = PosizioneService.getPositionsByIdUtente(resultSetUser.getString("idUtente"));
 				
 			Utente utente = new Utente(Integer.valueOf(resultSetUser.getString("idUtente")), resultSetUser.getString("email"), Boolean.valueOf((resultSetUser.getString("admin").equalsIgnoreCase("1")) ? "true" : "false"), resultSetUser.getString("nome"), resultSetUser.getString("cognome"), positions);
+			
+			resultSetUser.close();
+			con.commit();
+			return utente;
+		} catch (Exception e) {
+			
+			resultSetUser.close();
+			con.rollback();
+			throw e;
+		} finally {
+			
+			con.close();
+		}
+	}
+	
+	public static Utente getUserByIdUtenteWithProfile(Integer idUtente) throws Exception {
+		
+		Connection con = ConnectDatabase.getConnection();
+		
+		ResultSet resultSetUser = null;
+		try {
+			
+			con.setAutoCommit(false);
+			String SQLUser = "SELECT u.* FROM Utenti u WHERE u.idUtente = ?";
+			
+			PreparedStatement User = con.prepareStatement(SQLUser);
+			User.setString(1, idUtente.toString());
+			
+			resultSetUser = User.executeQuery();
+			
+			resultSetUser.next();
+			
+			ProfiloUtente userProfile = ProfiloUtenteService.getProfileUserByIdUtenteWithExperiencesAndEducation(Integer.valueOf(resultSetUser.getString("idUtente")));
+				
+			Utente utente = new Utente(Integer.valueOf(resultSetUser.getString("idUtente")), resultSetUser.getString("email"), Boolean.valueOf((resultSetUser.getString("admin").equalsIgnoreCase("1")) ? "true" : "false"), resultSetUser.getString("nome"), resultSetUser.getString("cognome"), userProfile);
 			
 			resultSetUser.close();
 			con.commit();
