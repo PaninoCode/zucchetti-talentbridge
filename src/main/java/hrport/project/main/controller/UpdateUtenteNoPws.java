fc1ee094e93910fffe8d9254c6b62cb975c10694
DarioDC95 <114399993+DarioDC95@users.noreply.github.com>
2023-11-21 17:58:43 +0100

    creation newServlet

--- src/main/java/hrport/project/main/controller/UpdateUtenteNoPws.java
+++ src/main/java/hrport/project/main/controller/UpdateUtenteNoPws.java
@@ -5,7 +5,12 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 
 /**
  * Servlet implementation class UpdateUtenteNoPws
@@ -19,6 +24,26 @@ public class UpdateUtenteNoPws extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		HttpSession session = request.getSession(false);
+		Integer idUtente = (Integer) session.getAttribute("idUtente");
+		
+		StringBuilder jsonContent = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(request.getInputStream()))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                jsonContent.append(line);
+            }
+        }catch(Exception e) {
+        	
+        	String error = "{\"data\" : " + "\"" + e.getMessage() + "\"" + "}";
+        	
+        	PrintWriter out = response.getWriter();
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            out.print(error);
+            out.flush();
+        }
 	}
 
 }
