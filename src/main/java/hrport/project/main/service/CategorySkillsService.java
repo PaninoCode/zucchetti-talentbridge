7f63118ca6d25219d8d6add5e8cce7ebaf7d22c5
DarioDC95 <114399993+DarioDC95@users.noreply.github.com>
2023-11-23 17:44:06 +0100

    categorySkill insert done

--- src/main/java/hrport/project/main/service/CategorySkillsService.java
+++ src/main/java/hrport/project/main/service/CategorySkillsService.java
@@ -1,11 +1,18 @@
 package hrport.project.main.service;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.time.LocalDate;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import hrport.project.main.adaptergson.LocalDateAdapter;
 import hrport.project.main.connectdb.ConnectDatabase;
 import hrport.project.main.pojo.CategoriaSkills;
 import hrport.project.main.pojo.Skill;
@@ -48,5 +55,60 @@ public class CategorySkillsService {
 		}
 		
 		return categories;
+	}
+	
+	public static void insertCategoryWithSkills(String json, Integer idCv) throws Exception {
+		
+		Connection con = ConnectDatabase.getConnection();
+		Gson gson = new GsonBuilder().registerTypeAdapter(LocalDate.class, new LocalDateAdapter()).create();
+		
+		ResultSet resultSetCategoryId = null;
+		CategoriaSkills category = null;
+
+		try {
+			
+			con.setAutoCommit(false);
+			
+			category = gson.fromJson(json, CategoriaSkills.class);
+					
+			String SQLCategory = "INSERT INTO \"CategoriaSkills\" (\"idCv\", \"nomeCategoria\")\r\n"
+					+ "VALUES (?, ?);"
+					+ "SELECT SCOPE_IDENTITY() as 'lastId'";
+			
+			PreparedStatement insertCategory = con.prepareStatement(SQLCategory);
+			
+			insertCategory.setInt(1, idCv);
+			insertCategory.setString(2, category.getNomeCategoria());
+			
+			resultSetCategoryId = insertCategory.executeQuery();
+			
+			String SQLSkills = "INSERT INTO \"Skill\" (\"idCs\", \"nomeSkill\")\r\n"
+					+ "VALUES (?, ?);"
+					+ "SELECT SCOPE_IDENTITY() as 'lastId'";
+			
+			resultSetCategoryId.next();
+			Integer lastId = resultSetCategoryId.getInt("lastId");
+			
+			for (Iterator<Skill> iterator = (category.getSkills()).iterator(); iterator.hasNext();) {
+				
+				Skill skill = iterator.next();
+				PreparedStatement insertSkill = con.prepareStatement(SQLSkills);
+				
+				insertSkill.setInt(1, lastId);
+				insertSkill.setString(2, skill.getNomeSkill());
+				
+				insertSkill.executeUpdate();
+			}
+			
+			con.commit();
+		} catch (Exception e) {
+			
+			con.rollback();
+			throw e;
+		} finally {
+			
+			con.close();
+		}
+			
 	}
 }
