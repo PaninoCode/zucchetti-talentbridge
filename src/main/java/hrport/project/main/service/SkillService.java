a56a62930087229f7e87d640695314ad5d063d93
DarioDC95 <114399993+DarioDC95@users.noreply.github.com>
2023-11-23 12:29:51 +0100

    categories get done

---
+++ src/main/java/hrport/project/main/service/SkillService.java
@@ -0,0 +1,49 @@
+package hrport.project.main.service;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.HashSet;
+import java.util.Set;
+
+import hrport.project.main.connectdb.ConnectDatabase;
+import hrport.project.main.pojo.Skill;
+
+public class SkillService {
+
+	public static Set<Skill> getSkillsByIdCs(Integer idCs) throws Exception {
+		
+		Connection con = ConnectDatabase.getConnection();
+		
+		ResultSet resultSetSkills = null;
+		Set<Skill> education = new HashSet<>();
+		
+		try {
+			
+			con.setAutoCommit(false);
+			String SQLUserPositions = "SELECT sk.* FROM Skill sk WHERE sk.idCs = ?";
+			
+			PreparedStatement userEducation = con.prepareStatement(SQLUserPositions);
+			userEducation.setInt(1, idCs);
+			
+			resultSetSkills = userEducation.executeQuery();
+			
+			while(resultSetSkills.next()) {
+				
+				education.add(new Skill(resultSetSkills.getInt("idCs"), resultSetSkills.getInt("idSkill"), resultSetSkills.getString("nomeSkill")));
+			}
+			
+			con.commit();
+		} catch (Exception e) {
+			
+			//throw e;
+			con.rollback();
+			education = new HashSet<>();
+		} finally {
+			
+			con.close();
+		}
+		
+		return education;
+	}
+}
